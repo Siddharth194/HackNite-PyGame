f91ecc0fe8addddb723923c0ec32fbd5a0dfcdb0
Rishabh-Dixit04 <117428785+Rishabh-Dixit04@users.noreply.github.com>
2023-04-16 02:45:00 +0530

    Add files via upload

---
+++ config (copy).py
@@ -0,0 +1,18 @@
+WIN_WIDTH = 900
+WIN_HEIGHT = 600
+
+FPS = 60
+
+LOADINGSCREENSPEED = 30 #higher the number, lower the speed
+LOADINGSCREENDELAY = 1 #higher the numnber, more the delay
+
+PLAYERSPEED = 4 #higher the number,slower the player
+FIGHTSPEED=6
+PLAYERVELOCITY = 3
+
+ENEMYSPEED=4
+ENEMYVELOCITY = 3
+
+LOADINGSCREENDONEFADEOUT = 1.5
+
+SIDE=90
