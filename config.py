e8ef7b42f46ee3e3b62aa6d25aba58d98533adce
Siddharth194 <83445812+Siddharth194@users.noreply.github.com>
2023-04-15 07:58:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 WIN_WIDTH = 900
 WIN_HEIGHT = 600
+
 FPS = 60
+
 LOADINGSCREENSPEED = 30 #higher the number, lower the speed
-LOADINGSCREENDELAY = 6 #higher the numnber, more the delay
+LOADINGSCREENDELAY = 4 #higher the numnber, more the delay
+
+PLAYERSPEED = 5 #higher the number,slower the player
+
+PLAYERVELOCITY = 16
+
+LOADINGSCREENDONEFADEOUT = 1.5
